3a62f1c67f92249d394ce45dee7b0d3d67e06272
Ali Maktabi <maktabi876@gmail.com>
2024-08-28 10:16:55 +0330

    modified cookie setting

--- src/utils/hooks/index.tsx
+++ src/utils/hooks/index.tsx
@@ -2,11 +2,11 @@ import { useEffect, useState } from "react"
 
 export const setCookie = (key: string, value: string) => {
   if (process.env.NODE_ENV === "development") {
-    document.cookie = `${key}=${value}; Path=/;`
-    // document.cookie = `${key}=${value}; Path=/;domain=api.wits.win; secure`
+    // document.cookie = `${key}=${value}; Path=/;`
+    document.cookie = `${key}=${value}; Path=/; domain=.wits.win`
   } else {
-    document.cookie = `${key}=${value}; Path=/; secure`
-    // document.cookie = `${key}=${value}; Path=/;domain=api.wits.win`
+    document.cookie = `${key}=${value}; Path=/; secure;`
+    document.cookie = `${key}=${value}; Path=/;domain=.wits.win; secure`
   }
 }
 
