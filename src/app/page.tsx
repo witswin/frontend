265e7dff919f9f4082ee77983c8221be066e016a
Ali Maktabi <maktabi876@gmail.com>
2024-09-07 11:17:54 +0330

    fixed landing page design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -120,7 +120,7 @@ const QuizEditor = () => {
 
 const ContentCardsSection = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 mt-28 gap-5 justify-center">
+    <div className="grid grid-cols-1 pb-28 md:grid-cols-2 mt-28 gap-5 justify-center">
       <CartItem
         className="col-span-1 md:col-span-2"
         icon={<FaMoneyCheckAlt size={30} />}
