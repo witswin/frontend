e60b7eb805afab260f3b420373258bf4a653a147
Ali Maktabi <maktabi876@gmail.com>
2024-09-28 12:40:37 +0330

    added save button

--- src/app/(app)/dashboard/quizzes/create/_components/header.tsx
+++ src/app/(app)/dashboard/quizzes/create/_components/header.tsx
@@ -1,19 +1,25 @@
 "use client"
 
+import { Button } from "@nextui-org/react"
 import { useQuizCreateContext } from "../providers"
 import TextInput from "./text-input"
+import { FaSave } from "react-icons/fa"
 
 const Header = () => {
   const { control } = useQuizCreateContext()
 
   return (
     <header className="quiztap-header rounded-xl border-2 border-gray20 p-5">
-      <TextInput
-        size="large"
-        control={control!}
-        name="title"
-        placeholder="Quiz Title"
-      />
+      <div className="flex items-center justify-between">
+        <TextInput
+          size="large"
+          control={control!}
+          name="title"
+          placeholder="Quiz Title"
+        />
+
+        <Button startContent={<FaSave />}>Save</Button>
+      </div>
       <TextInput
         className="mt-3"
         control={control!}
